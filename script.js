f71f42e32ed7685ca30137404e187baaaf3cece5
LightmanBro <omotosodavidromeo@gmail.com>
2024-02-19 13:48:00 +0100

    added js styles

--- script.js
+++ script.js
@@ -134,10 +134,17 @@ image.onload = function () {
         // Touch events are supported
         // canvas.width=screen.width;
         console.log(navigator.platform,canvas.width)
+        canvas.style.display = 'flex';
+        canvas.style.maxWidth = '100vw';
+        canvas.style.boxSizing = 'border-box';
         canvas.width =5400;
+        image.style.flex = '1 0 auto';
+        image.style.objectFit = 'cover';
+        
         image.style.width =5400;
         canvas.height = 7200
         image.style.height = 7200;
+
         document.querySelector('.dimension').innerHTML = `2 image-width${image.width} : canvas-width ${canvas.width} image-height${image.height} : canvas.height ${canvas.height} : ${screen.width} ${screen.height}`
         console.log(`2 image-width${image.width} : canvas-width ${canvas.width} image-height${image.height} : canvas.height ${canvas.height}, ${screen.width} ${screen.height}`)
     } else {
