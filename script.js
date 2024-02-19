ff5214774d9fdb4bf410c67cbb3bd5c414cdcf0d
LightmanBro <omotosodavidromeo@gmail.com>
2024-02-19 14:05:42 +0100

    ...

--- script.js
+++ script.js
@@ -141,9 +141,9 @@ image.onload = function () {
         image.style.flex = '1 0 auto';
         image.style.objectFit = 'cover';
 
-        image.style.width =5400;
+        image.width =5400;
         canvas.height = 7200
-        image.style.height = 7200;
+        image.height = 7200;
 
         document.querySelector('.dimension').innerHTML = `2 image-width${image.width} : canvas-width ${canvas.width} image-height${image.height} : canvas.height ${canvas.height} : ${screen.width} ${screen.height}, js css 100vh`
         console.log(`2 image-width${image.width} : canvas-width ${canvas.width} image-height${image.height} : canvas.height ${canvas.height}, ${screen.width} ${screen.height}`)
